c2362874259be16b8ade8f78a2ac2059d891bc0a
perlancar (@pc-home) <perlancar@gmail.com>
2016-02-21 22:53:32 +0700

    Initial commit

---
+++ lib/Acme/CPANLists/Import.pm
@@ -0,0 +1,21 @@
+package Acme::CPANLists::Import;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: CPANLists imported from various web pages
+
+=head1 DESCRIPTION
+
+This distribution contains module lists that are imported from various web
+pages.
+
+
+=head1 append:SEE ALSO
+
+L<App::lcpan> and L<lcpan>, particularly the B<related-mods> subcommand.
+
+Other C<Acme::CPANLists::Import::*> modules, e.g.
+L<Acme::CPANLists::Import::NEILB>, L<Acme::CPANLists::Import::SHARYANTO>,
+L<Acme::CPANLists::Import::RSAVAGE>.
